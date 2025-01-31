dd9be5f66d40b570565037567fcb2cdf8f776535
Kamrul Islam <kamrulislamabk@gmail.com>
2025-01-31 22:32:56 +0600

    Add Button component and update styles; include clsx and tailwind-merge dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import MainLayout from "./components/layout/MainLayout";
+import Button from "./components/ui/Button";
 import Container from "./components/ui/Container";
 
 function App() {
@@ -6,12 +7,13 @@ function App() {
   return (
     <Container>
       <div className="h-screen w-full flex items-center justify-center">
-        <button className="px-3 py-2 bg-purple-400 rounded-md">
-          This is a button
-        </button>
-        <button className="px-3 py-2 bg-purple-400 rounded-md">
-          This is a button 2
-        </button>
+        {/* <h1>This is a h1 tag</h1>
+        <h2>This is a h1 tag</h2>
+        <h3>This is a h1 tag</h3>
+        <button className="btn btn-primary">This is a button</button>
+        <button className="btn btn-danger">This is a button 2</button> */}
+
+        <Button outline={true} />
       </div>
     </Container>
   );
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+import cn from "../../utils/cn";
+
+// `bg-purple-500 px-3 py-2 rounded-md ${className} ${
+//   variant == "outline" ? "border border-purple-500 bg-opacity-10" : ""
+// }`;
+
+const Button = ({ className, outline }) => {
+  return (
+    <button
+      className={cn(
+        "bg-purple-500 px-3 py-2 rounded-md",
+        {
+          "border border-purple-500 bg-opacity-10": outline,
+        },
+        className
+      )}
+    >
+      Click
+    </button>
+  );
+};
+
+export default Button;
